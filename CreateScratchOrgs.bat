7c9a07b8a2c564c7029a07e305b9649ed62fd9cd
StefanBotman <stefanbotman@gmail.com>
2018-07-05 12:42:59 +0200

    initial commit

---
+++ CreateScratchOrgs.bat
@@ -0,0 +1,3 @@
+sfdx force:org:create -s -f config/project-scratch-dev.json -a CICDDev
+sfdx force:org:create -s -f config/project-scratch-test.json -a CICDTest
+sfdx force:org:create -s -f config/project-scratch-prod.json -a CICDProd
